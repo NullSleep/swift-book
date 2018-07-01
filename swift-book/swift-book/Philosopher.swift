a86d2b7473b4af25d4731b5238025ec8c7429210
Carlos Arenas <carenasdev@gmail.com>
2018-07-01 00:10:06 -0500

    Adding supporting classes.

---
+++ swift-book/swift-book/Philosopher.swift
@@ -0,0 +1,15 @@
+//
+//  Philosopher.swift
+//  Philosophers
+//
+//  Created by Carlos Arenas on 5/24/18.
+//  Copyright © 2018 Carlos Arenas. All rights reserved.
+//
+
+import Foundation
+
+class Philosopher {
+    var name: String?
+    var image: String?
+    var cellImage: String?
+}
