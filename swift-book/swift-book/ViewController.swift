6e3dbcccc73ae5b0345a1f5332a2b70971e34d11
Carlos Arenas <carenasdev@gmail.com>
2018-06-30 23:40:50 -0500

    Project skeleton

---
+++ swift-book/swift-book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-book
+//
+//  Created by Carlos Arenas on 6/30/18.
+//  Copyright © 2018 Carlos Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
