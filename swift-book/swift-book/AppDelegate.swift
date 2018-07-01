d00e36fb057cce9ab12b1f5fde1a608d6b275455
Carlos Arenas <carenasdev@gmail.com>
2018-07-01 00:11:56 -0500

    Running the project.

--- swift-book/swift-book/AppDelegate.swift
+++ swift-book/swift-book/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    private var  chapterOne = chapter1()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        chapterOne.coreFeatures()
+        
         return true
     }
 
