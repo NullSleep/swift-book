40dc1dea8b8d4c8ceac30c4e87e25db606a2bce3
Carlos Arenas <carenasdev@gmail.com>
2018-07-03 23:54:13 -0500

    Tuples and optionals documentation.

--- swift-book/swift-book/Chapters/chapter2.swift
+++ swift-book/swift-book/Chapters/chapter2.swift
@@ -17,6 +17,14 @@ class chapter2 {
     // - var arr:[Int] = [] // Array with an array literal
     // - var arr:Array<Int> = []
     
+    // Tuples enable to create and pass around groupings of values. You can use a tuple  to return multiple
+    // values from a function as a single compund value.
+    
+    // Swift introduces optional types, which handle the absence of a value. Optionals say either "there is a value, and
+    // it equals x" or "there isn't a value at all". Using optional is similar to using nil pointers in Obj-C but
+    // they work for any type, not just classes. Not only are optionals safer and more expressive than nil pointer in
+    // Obj-C they are at the heart of many Swift's most powerful features
+    
     // MARK: - Sub Chapter: The Basics
     
     func execute() {
