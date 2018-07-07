d17edd367c5972d473471c1d77095bf8efbad9f7
Carlos Arenas <carenasdev@gmail.com>
2018-07-06 23:39:23 -0500

    Type safety and type inference documentation.

--- swift-book/swift-book/Chapters/chapter2.swift
+++ swift-book/swift-book/Chapters/chapter2.swift
@@ -113,6 +113,11 @@ class chapter2 {
         // Floating-point numbers are numbers with a fractional component (3.141559, 0.1, -356.75)
         // Swift provides two signed floating-point number types:
         // Double represents a 64-bit floating-point number. Has the precision of at least 15 decimal digits
-        // Float represents a 32-bit floating-point number. Has the precision of at least 6 decimal digits
+        // Float represents a 32-bit floating-point number. Has the precision of at least 6 decimal digits.
+        
+        // Type Safety and Type Inference
+        // Swift is a type-sage language meaning that it encourages you to be clear about the types the code can work with.
+        // If part of you code expects a String, you can't pass in an Int by mistake.
+        // Swift inferes values using type inference to work out the appropiate type.
     }
 }
