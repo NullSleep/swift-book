ec2ea3abaf792cdce3792a9140e2ec0c526f6d8e
Carlos Arenas <carenasdev@gmail.com>
2018-07-16 23:38:40 -0500

    Basic operators documentation.

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -12,7 +12,13 @@ class chapter2_2 {
     
     // MARK: - SUB CHAPTER: BASIC OPERATORS
     
+    // An operator is a special symbol or phrase that is used to check, change or combine values.
+    
     func execute() {
         
+        // ** Terminology **
+        // · Unary - Operates on a single target: -a, !b, c!
+        // · Binary - Operates on two targets: 2 + 3
+        // · Ternary - Operates on three targets. Swift has only one ternary operator: a ? b : c
     }
 }
