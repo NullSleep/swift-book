46f9061d71dd7177d84821ebbb48ac7a577260f8
Carlos Arenas <carenasdev@gmail.com>
2018-07-17 23:49:24 -0500

    Arithmetic Operators

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -32,5 +32,13 @@ class chapter2_2 {
         print((x, y))
         print("\n---\n")
         
+        // ** Arithmetic Operators **
+        print(1 + 2)
+        print(5 - 3)
+        print(2 * 3)
+        print(10.0 / 2.5)
+        
+        // Unlike arithmetic operators in C and Obj-C, the Swift arithmetic operatos do not allow values to overflow
+        // by default.
     }
 }
