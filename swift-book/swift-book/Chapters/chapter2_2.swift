e61e0c8a3da2f28743279c1da01dae821b115481
Carlos Arenas <carenasdev@gmail.com>
2018-07-19 23:52:20 -0500

    Comparing tuples.

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -103,5 +103,11 @@ class chapter2_2 {
         } else {
             print("If you want to grow up, you have to stop apologizing.")
         }
+        
+        // Tuple that have the same number of values can be compared as long as each of the values in the tuple can be
+        // compared. For example both Int and String can be compared, which means tuples of the type (Int, String)
+        // can be comapted. In contrast Bool can't be compared, which means tuples that contain a Boolean value
+        // can't be compared.
+        print((1, "zebra") < (2, "apple"))
     }
 }
