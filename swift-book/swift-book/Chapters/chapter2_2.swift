fd3297b1e710671ce8917804fb262f19e050bcbd
Carlos Arenas <carenasdev@gmail.com>
2018-07-19 23:56:20 -0500

    Another tuple comparison.

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -110,5 +110,6 @@ class chapter2_2 {
         // can't be compared.
         print((1, "zebra") < (2, "apple")) // true because 1 is less than 2; "zebra" and "apple" are not compared
         print((3, "apple") < (3, "bird")) // true because 3 is equal to 3, and "apple" is less than "bird"
+        print((4, "dog") == (4, "dog")) // true because 4 is equal 4 and "dog" is equal to "dog"
     }
 }
