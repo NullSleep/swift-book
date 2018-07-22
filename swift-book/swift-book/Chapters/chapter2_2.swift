cdee9b43bf4c717b57b548c3442c68ca21382e26
Carlos Arenas <carenasdev@gmail.com>
2018-07-21 23:55:35 -0500

    Fixing an error in the loop.

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -176,7 +176,7 @@ class chapter2_2 {
         // * Closed range operator *
         // The closed range operator (a..b) defines a range that runs from a to b, and includes the values a and b. The
         // value of a must not be greater than b.
-        for index in 1..5 {
+        for index in 1...5 {
             print("\(index) times 5 is \(index * 5)")
         }
     }
