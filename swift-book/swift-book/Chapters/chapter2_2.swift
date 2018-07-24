c5092e748f9abe462dd204611e3e755fbfc755d6
Carlos Arenas <carenasdev@gmail.com>
2018-07-23 23:41:24 -0500

    One-sided ranges documentation and examples.

--- swift-book/swift-book/Chapters/chapter2_2.swift
+++ swift-book/swift-book/Chapters/chapter2_2.swift
@@ -173,14 +173,16 @@ class chapter2_2 {
         
         // ** Range Operators **
         // Swift includes serveral range operators which are shortcuts for expressing a range of values.
+        
         // * Closed range operator *
         // The closed range operator (a..b) defines a range that runs from a to b, and includes the values a and b. The
         // value of a must not be greater than b.
         for index in 1...5 {
             print("\(index) times 5 is \(index * 5)")
         }
+        print("\n---\n")
         
-        // ** Half Range Operator **
+        // * Half Range Operator *
         // The half range operator (a..<b) defines a range that runs from a to b, but does not include b. The is called
         // half open because it contains its first value, but not the final value.
         // Half open ranges are useful when working with zero based lists such as arrays, where it is useful to count up
@@ -189,6 +191,18 @@ class chapter2_2 {
         let count = names.count
         for i in 0..<count {
             print("Person \(i + 1) is called \(names[i])")
+        }
+        print("\n---\n")
+        
+        // * One-Sided Ranges*
+        // The closed range operator has an alternate form for ranges that continue as far as possible in one direction,
+        // for example, a range that inclides all the elements of an array, from index 2 to the end of the array.
+        for name in names[2...] {
+            print(name)
+        }
+        
+        for name in names[...2] {
+            print(name)
         }
     }
     
