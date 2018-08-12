45b70a40bac4a1b9049a73c08962622fc71c2923
Carlos Arenas <carenasdev@gmail.com>
2018-08-12 02:45:34 -0500

    Collection types documentation.

--- swift-book/swift-book/Chapters/chapter2_4.swift
+++ swift-book/swift-book/Chapters/chapter2_4.swift
@@ -12,5 +12,15 @@ class chapter2_4 {
     // MARK: - SUB CHAPTER: COLLECTION TYPES
     
      func execute() {
+        
+        // Swift provide three primary collection types, known as arrays, sets, and dictionaries, for sorting colelctions
+        // of values. Array are ordered collections of values. Sets are unordered collections of unique values.
+        // Dictionaries are unordered collections og key-value associations.
+        
+        // Arrays, sets, and dictionaries in Swift are always clear about the types of values and keys that they can store.
+        // This means that you cannot insert a value of the worng type into a collection by mistake. It also means you
+        // can be condifent about the type of values you will retrieve from a colleciton.
+        
+        // Swift's array, set and dictionary type are implemented as generic collection
     }
 }
