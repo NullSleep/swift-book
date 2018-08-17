89ad0bb64052f1f4304c37429584e96479a08bb3
Carlos Arenas <carenasdev@gmail.com>
2018-08-17 16:09:56 -0500

    Documentation of collection types and arrays.

--- swift-book/swift-book/Chapters/chapter2_4.swift
+++ swift-book/swift-book/Chapters/chapter2_4.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class chapter2_4 {
-    // MARK: - SUB CHAPTER: COLLECTION TYPES
+    // MARK: - CHAPTER: COLLECTION TYPES
     
      func execute() {
         
@@ -23,9 +23,24 @@ class chapter2_4 {
         
         // Swift's array, set and dictionary type are implemented as generic collectionns.
         
-        // ** Mutability of Collections **
+        // MARK: - MUTABILITY OF COLLECTIONS
         // If you create an array, set, or a dictionary, and assign it to a variable, the collection that is created will
         // be mutalbe. This means that you can change (or mutate) the collection after it is created by adding, removing,
-        // or changing iterms in the collection.
+        // or changing iterms in the collection. If you assign an array, a set, or a dictionry to a constant, that
+        // collection is immutable, and its size and contents cannot be change.
+        
+        // It is good practice to create immutable collections in all cases where the collection does not need to change.
+        // Doing so makes it easier for you to reason about your code and enables the Swift compiler to optimze the
+        // performance of the collections you create.
+        
+        // MARK: - ARRAYS
+        // An array stores values of the same type in an ordered list. The same value can appear in an array multiple
+        // times at different positions. Swift's Array type is briged to Foundation's NSArray class.
+        
+        // MARK: Array Type Shorthand Syntax
+        // The type of a Swift array is written in full as Array<Element>, where Element is the type of values the array
+        // is allow to store. You can also wirte the type of an array in shorthand for as [Element]. Although the two forms
+        // are functionally identical, the shorthand form is prefered and is used throughout this guide when referring to
+        // the type of an arrat.
     }
 }
