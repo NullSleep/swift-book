ea3ec0bd87c54554859942d7d61ac50f81306bcd
Carlos Arenas <carenasdev@gmail.com>
2018-09-05 00:30:09 -0500

    Examples of for loops

--- swift-book/swift-book/Model/Chapters/chapter2_5.swift
+++ swift-book/swift-book/Model/Chapters/chapter2_5.swift
@@ -48,5 +48,21 @@ class chapter2_5 {
         for index in 1...5 {
             print("\(index) times 5 is \(index * 5)")
         }
+        
+        // If you don't need each value from a sequence, you can ignore the values by using an underscore in place of a
+        // variable name.
+        let base = 3
+        let power = 10
+        var answer = 1
+        for _ in 1...power {
+            answer *= base
+        }
+        print("\(base) to the power of \(power) is \(answer)")
+        
+        // Use the half-open range operator (..<) to include the lower bound but not the upper bound.
+        let minutes = 60
+        for tickMark in 0..<minutes {
+            print("Render the tick mark [\(tickMark)] each minute (60 times")
+        }
     }
 }
