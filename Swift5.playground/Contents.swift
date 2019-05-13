aefbc5e7aeb29a2348eea4244b5a3e851717ac89
Carlos Arenas <nullsleep@me.com>
2019-05-13 15:00:43 -0500

    What's new in Swift 5.0

---
+++ Swift5.playground/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+
+ # What's new in Swift 5.0
+ 
+ * Based from the following [repo](sourcerepo)
+ 
+ This playground is designed to showcase new features introduced with Swift 5.0.
+ 
+ ## Contents
+ * [Raw strings]
+ 
+*/
+
+import UIKit
+
+var str = "Hello, playground"
